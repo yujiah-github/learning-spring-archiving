3ef1710a17661e55a3634f8b5adbb7f6b726d892
yu <78803363+yujiah-github@users.noreply.github.com>
2021-08-17 23:50:39 +0900

    210817 Correct HTTPResponse Sources

--- servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -1,36 +1,41 @@
 package hello.servlet.basic.response;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//application/json 은 스펙상 utf-8 형식을 사용하도록 정의되어 있다. 그래서 스펙에서 'charset=utf-8'과 같은 추가 파라미터를 지원하지 않는다.
+// 따라서 'application/json' 이라고만 사용해야지 'application/json;charset=utf-8' 이라고 전달하는 것은 의미 없는 파라미터를 추가한 것이 된다.
+//'response.getWriter()'를 사용하면 추가 파라미터를 자동으로 추가해버린다.
+// 이때는 'response.getOutputStream()'으로 출력하면 그런 문제가 없다.
+
+//HTTP 응답으로 JSON을 반환할 때는 content-type을 application/json 로 지정해야 한다.
+// Jackson 라이브러리가 제공하는 objectMapper.writeValueAsString() 를 사용하면 객체를 JSON 문자로 변경할 수 있다.
 
 @WebServlet(name = "responseJsonServlet",urlPatterns = "/response-json")
 public class ResponseJsonServlet extends HttpServlet {
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        response.setHeader("content-type","application/json");
+        response.setCharacterEncoding("utf-8");
 
+        helloData data = new HelloData();
+        data.setUsername("kim");
+        data.setAge(20);
 
+        String result = objectMapper.writeValueAsString(data);
 
+        response.getWriter().write(result);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
