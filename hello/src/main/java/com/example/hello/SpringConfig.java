f9b0de134998fd14c1fa532f25984ee5cc5f3908
yu <78803363+yujiah-github@users.noreply.github.com>
2021-08-30 23:38:24 +0900

    210830 Add JDBC connection and JDBC Repository

--- hello/src/main/java/com/example/hello/SpringConfig.java
+++ hello/src/main/java/com/example/hello/SpringConfig.java
@@ -1,17 +1,33 @@
 package com.example.hello;
 
+import com.example.hello.repository.JdbcMemberRepository;
 import com.example.hello.repository.MemberRepository;
 import com.example.hello.repository.MemoryMemberRepository;
 import com.example.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+
+@Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){ return new MemberService(memberRepository());}
 
     @Bean
-    public MemberRepository memberRepository(){ return new MemoryMemberRepository(); }
+    public MemberRepository memberRepository()
+    {
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+    }
 }
--- hello/src/main/java/com/example/hello/repository/MemberRepository.java
+++ hello/src/main/java/com/example/hello/repository/MemberRepository.java
@@ -2,18 +2,19 @@ package com.example.hello.repository;
 
 import com.example.hello.domain.Member;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member);
+    Member save(Member member) throws SQLException;
 
     Optional<Member> findById(Long id);
 
-    Optional<Member> findByName(String name);
+    Optional<Member> findByName(String name) throws SQLException;
 
     //자바 8기능 > 없으면 널로 반환
-    List<Member> findAll();
+    List<Member> findAll() throws SQLException;
 
     void clearStore();
 }
