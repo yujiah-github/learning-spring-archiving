2176fc4ff3e77a9d260cbbc0d33a5780f5dcbd08
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-02 00:00:40 +0900

    210901 ADD sourcefiles Rewrite README.md

---
+++ hello/src/main/java/com/example/hello/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.hello.repository;
+
+import com.example.hello.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member,Long>, MemberRepository{
+
+    Optional<Member> findByName(String name);
+}
