a54cf537896b3d57724fff3673641a138d2e6abf
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-04 20:45:33 +0900

    210904 Add Orderservice and servicetest sourcefiles

--- core 2/src/test/java/hello/core/member/MemberServiceTest.java
+++ core 2/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,7 +1,6 @@
 package hello.core.member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MemberServiceTest {
     MemberService memberService = new MemberServiceImpl();
