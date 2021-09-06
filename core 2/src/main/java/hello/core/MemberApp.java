0972eba82b37ab7749a6bed3c8d85120e2cd41b8
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-06 15:32:44 +0900

    210906 Add appconfig source and Sort mechanism

--- core 2/src/main/java/hello/core/MemberApp.java
+++ core 2/src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-
-import java.util.Arrays;
+import hello.core.order.OrderService;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+
         Member member = new Member(1L,"memberA", Grade.VIP);
         memberService.join(member);
 
--- core 2/src/main/java/hello/core/OrderApp.java
+++ core 2/src/main/java/hello/core/OrderApp.java
@@ -3,17 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
-
-import java.util.Arrays;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         long memberId = 1L;
 
