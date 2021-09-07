bdb88eac75dc3f7e20d58c8a6ae135c0563decf9
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-07 16:31:59 +0900

    210907 Add srping code

--- core 2/src/main/java/hello/core/OrderApp.java
+++ core 2/src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,20 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+       /* AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        OrderService orderService = appConfig.orderService(); */
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         long memberId = 1L;
 
