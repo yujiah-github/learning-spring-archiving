0ff90fa36f73ec62892b7c62494d7098455972f4
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-13 14:23:12 +0900

    210913 Add stateless singleton source code

---
+++ core 2/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,35 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class StatefulServiceTest {
+    @Test
+    void statefulServiceSingleton(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+       int userAprice =  statefulService1.order("userA", 10000);
+       int userBprice =  statefulService2.order("userB", 20000);
+
+       // int price = statefulService1.getPrice();
+
+        System.out.println("price= " +userAprice);
+
+        // assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+}
