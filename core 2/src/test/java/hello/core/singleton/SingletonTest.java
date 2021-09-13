712c21cde8f0d017f1a2415fdd640c116cf98acc
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-13 11:45:22 +0900

    210913 Add spring container test code sourcefiles

--- core 2/src/test/java/hello/core/singleton/SingletonTest.java
+++ core 2/src/test/java/hello/core/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package hello.core.singleton;
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,4 +24,43 @@ public class SingletonTest {
 
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void SingletonServiceTest(){
+
+        SingletonService singletonService1 = SingletonService.getInstance();
+
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+     System.out.println("singletonService1= " +singletonService1);
+      System.out.println("singletonService2= " +singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+
+      singletonService1.logic();
+    }
+
+
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+
+        System.out.println("memberService1= "+ memberService1);
+        System.out.println("memberService2= "+ memberService2);
+
+
+        assertThat(memberService1).isSameAs(memberService2);
+
+
+    }
+
+
 }
