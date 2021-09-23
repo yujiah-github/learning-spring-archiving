f4f7f2f0fc43c9dda638728a081b6932de52e57b
yu <78803363+yujiah-github@users.noreply.github.com>
2021-09-23 17:32:54 +0900

    210923 Bean Conflict

--- core 2/src/main/java/hello/core/AutoAppConfig.java
+++ core 2/src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,6 +13,10 @@ import org.springframework.context.annotation.FilterType;
         basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
-public class AutoAppConfig {
+public class AutoAppConfig { //자동 등록과 수동 등록이 붙으면 수동 등록이 이긴다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
