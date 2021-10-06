d24951bcfa990963cda16af82446d920d6fd8681
yu <78803363+yujiah-github@users.noreply.github.com>
2021-10-06 10:16:39 +0900

    211006 Add Lombok library

--- core 2/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core 2/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,10 +3,11 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
@@ -14,12 +15,6 @@ public class OrderServiceImpl implements OrderService{
 //테스트 용도
     public MemberRepository getMemberRepository(){
         return memberRepository;
-    }
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
     }
 
     @Override
